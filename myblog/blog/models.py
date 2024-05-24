3d19914aa495e736caef2ae2df4db737435d1f58
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-05-24 15:09:30 +0300

    Modified blog/models

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -3,21 +3,15 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxLengthValidator
 from django.core.exceptions import ValidationError
 
-# Create your models here.
+
 class BlogPost(models.Model):
-    title = models.CharField(max_length = 100,)
-    body = models.TextField(validators = [MaxLengthValidator(255)])
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 100)
+    body = models.TextField(validators=[MaxLengthValidator(255)])
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return self.title #return the title of the post as a display of the model
+        return self.title
     
-    def clean(self):
-        if not self.title:
-            raise ValidationError("Title can't be empty")
-        if not self.body:
-            raise ValidationError("Body can't be empty") #Implement the following validations: title and body cannot be empty (But they cannot be empty anyway)
-        
     class Meta:
         verbose_name = 'Post'  
         verbose_name_plural = 'Posts'  
