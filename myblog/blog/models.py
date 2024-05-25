6d0836359a62ec097db40f1704a182bb8acc46f4
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-05-25 17:24:11 +0300

    Complete

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxLengthValidator
-from django.core.exceptions import ValidationError
 
 
 class BlogPost(models.Model):
--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+import pytest
+from django.forms import ValidationError
+from django.contrib.auth.models import User
+from blog.models import BlogPost
 
-# Create your tests here.
+@pytest.mark.django_db
+class TestPostModel:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, db):
+        self.user = User.objects.create_user(username="testuser", password="1234")
+
+    def test_title_cannot_be_empty(self):
+        post = BlogPost(title='', body='Some body text', author=self.user)
+        with pytest.raises(ValidationError):
+            post.full_clean()  # This will run the model's clean methods
+
+    def test_body_cannot_be_empty(self):
+        post = BlogPost(title='Some title', body='', author=self.user)
+        with pytest.raises(ValidationError):
+            post.full_clean()
+
+    def test_title_max_length(self):
+        post = BlogPost(title='a' * 101, body='Some body text', author=self.user)
+        with pytest.raises(ValidationError):
+            post.full_clean()
+
+    def test_body_max_length(self):
+        post = BlogPost(title='Some title for test', body='X' * 256, author=self.user)
+        with pytest.raises(ValidationError):
+            post.full_clean()
