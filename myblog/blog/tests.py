ca1fadbc822853a53551d3ff49d75e9a4f3c5e5b
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-05-25 16:53:27 +0300

    Rewrite tests to use pytest instead of unittest

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -1,32 +1,30 @@
 import pytest
 from django.forms import ValidationError
-from django.test import TestCase
 from django.contrib.auth.models import User
 from blog.models import BlogPost
 
-
 @pytest.mark.django_db
-class TestPostModel(TestCase):
-    def setUp(self):
+class TestPostModel:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, db):
         self.user = User.objects.create_user(username="testuser", password="1234")
 
     def test_title_cannot_be_empty(self):
         post = BlogPost(title='', body='Some body text', author=self.user)
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             post.full_clean()  # This will run the model's clean methods
 
     def test_body_cannot_be_empty(self):
         post = BlogPost(title='Some title', body='', author=self.user)
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             post.full_clean()
 
     def test_title_max_length(self):
         post = BlogPost(title='a' * 101, body='Some body text', author=self.user)
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             post.full_clean()
 
     def test_body_max_length(self):
         post = BlogPost(title='Some title for test', body='X' * 256, author=self.user)
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             post.full_clean()
-
