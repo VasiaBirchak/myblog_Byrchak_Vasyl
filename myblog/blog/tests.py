612a8b13af60842d26c8f99430360b6eacba9565
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-05-30 19:32:22 +0300

    Reworked with recommendations

--- myblog/blog/tests.py
+++ myblog/blog/tests.py
@@ -6,27 +6,28 @@ from blog.models import BlogPost
 
 @pytest.mark.django_db
 class TestPostModel:
-    @pytest.fixture(autouse=True)
-    def setup_method(self, db):
-        self.user = User.objects.create_user(username="testuser", password="1234")
+    @pytest.fixture()
+    def user(self, db):
+        return User.objects.create_user(username="testuser", password="1234")
 
-    def test_title_cannot_be_empty(self):
-        post = BlogPost(title='', body='body', author=self.user)
+
+    def test_title_cannot_be_empty(self, user):
+        post = BlogPost(title='', body='body', author=user)
         with pytest.raises(ValidationError):
             post.full_clean()
 
-    def test_body_cannot_be_empty(self):
-        post = BlogPost(title='title', body='', author=self.user)
+    def test_body_cannot_be_empty(self, user):
+        post = BlogPost(title='title', body='', author=user)
         with pytest.raises(ValidationError):
             post.full_clean()
 
-    def test_title_max_length(self):
-        post = BlogPost(title='a' * 101, body='body', author=self.user)
+    def test_title_max_length(self, user):
+        post = BlogPost(title='a' * 101, body='body', author=user)
         with pytest.raises(ValidationError):
             post.full_clean()
 
-    def test_body_max_length(self):
-        post = BlogPost(title='title', body='X' * 256, author=self.user)
+    def test_body_max_length(self, user):
+        post = BlogPost(title='title', body='X' * 256, author=user)
         with pytest.raises(ValidationError):
             post.full_clean()
 
