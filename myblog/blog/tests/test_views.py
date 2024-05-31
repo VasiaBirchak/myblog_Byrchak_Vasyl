fa3f5b9ff5bbc707feb95c26c4660cbced6ce911
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-05-31 15:08:02 +0300

    Added tests

---
+++ myblog/blog/tests/test_views.py
@@ -0,0 +1,28 @@
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+@pytest.mark.django_db
+class TestUserAuthViews:
+    @pytest.fixture()
+    def user(self, db):
+        return User.objects.create_user(username="testuser", password="1234")
+
+
+    def test_login_page(self, client):
+        url = reverse('user_login')
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+    def test_register_page(self, client):
+        url = reverse('user_reg')
+        response = client.get(url)
+        assert response.status_code == 200
+
+
+    def test_logout_page(self, client, user):
+        client.login(username='testuser', password='testpassword')
+        url = reverse('logout')
+        response = client.get(url)
+        assert response.status_code == 302
