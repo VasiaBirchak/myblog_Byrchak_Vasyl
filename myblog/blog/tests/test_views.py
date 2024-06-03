3f645930cf8f03e560ee7c093c6e82998d2c0239
Vasyl <vasyl@Vasyls-MacBook-Pro.local>
2024-06-03 15:41:43 +0300

    Added more functionality to the test_views.py and redesigned the registration so that the user is immediately logged in after successful registration

--- myblog/blog/tests/test_views.py
+++ myblog/blog/tests/test_views.py
@@ -9,18 +9,35 @@ class TestUserAuthViews:
     def user(self, db):
         return User.objects.create_user(username="testuser", password="1234")
 
-    def test_login_page(self, client):
+    def test_login_page(self, client, user):
         url = reverse('user_login')
         response = client.get(url)
         assert response.status_code == 200
+
+        response = client.post(url, {'username': 'testuser', 'password': '1234'})
+        assert response.status_code == 302
+        assert '_auth_user_id' in client.session
+        
+        user_id_in_session = client.session['_auth_user_id']
+        logged_in_user = User.objects.get(pk=user_id_in_session)
+        assert logged_in_user.username == 'testuser'
 
     def test_register_page(self, client):
         url = reverse('user_reg')
         response = client.get(url)
         assert response.status_code == 200
+        response = client.post(url, {
+            'username': 'newuser',
+            'password1': 'complexpassword',
+            'password2': 'complexpassword',
+            'email': 'newuser@example.com'
+        })
+        assert response.status_code == 302
+        assert '_auth_user_id' in client.session
 
     def test_logout_page(self, client, user):
         client.login(username='testuser', password='testpassword')
         url = reverse('logout')
         response = client.get(url)
         assert response.status_code == 302
+        assert '_auth_user_id' not in client.session
